1e553bb1a1e22c8e7bf7ae4086f00de55696c0a8
Nipuna-Sankalpa <nipunasankalpa.12@cse.mrt.ac.lk>
2020-09-08 21:48:43 +0530

    directory path correction

--- remove-databases/main.py
+++ remove-databases/main.py
@@ -8,26 +8,23 @@ def get_legitimate_db_list(webroot):
     isExist = os.path.exists(webroot)
     if not isExist:
         return "Web root does not exist"
-    for dirname, dirnames_first_level, filenames in os.walk(webroot):
-        print(dirnames_first_level)
-        for directory_first_level in dirnames_first_level:
-            secondary_webroot = webroot + '/' + directory_first_level
-            print(secondary_webroot)
-            for dirname, secondary_directory, filenames in os.walk(secondary_webroot):
-                for directory in secondary_directory:
-                    db_yml_path = webroot + '/' + directory_first_level + '/' + directory + "/symfony/config/databases.yml"
-                    isExist = os.path.exists(db_yml_path)
-                    if isExist:
-                        with open(db_yml_path, "r") as configuration:
-                            db_settings = yaml.load(configuration, yaml.SafeLoader)
-                            try:
-                                tmp = db_settings['all']['doctrine']['param']['dsn']
-                                literals = tmp.split(";")
-                                db_name = literals[2].split("=")[1]
-                                print(db_name)
-                                db_array.append(db_name)
-                            except yaml.YAMLError as error:
-                                print(error)
+    for directory_first_level in os.listdir(webroot):
+        secondary_web_root = webroot + '/' + directory_first_level
+        print(secondary_web_root)
+        for directory in os.listdir(secondary_web_root):
+            db_yml_path = webroot + '/' + directory_first_level + '/' + directory + "/symfony/config/databases.yml"
+            isExist = os.path.exists(db_yml_path)
+            if isExist:
+                with open(db_yml_path, "r") as configuration:
+                    db_settings = yaml.load(configuration, yaml.SafeLoader)
+                    try:
+                        tmp = db_settings['all']['doctrine']['param']['dsn']
+                        literals = tmp.split(";")
+                        db_name = literals[2].split("=")[1]
+                        print(db_name)
+                        db_array.append(db_name)
+                    except yaml.YAMLError as error:
+                        print(error)
 
     return db_array
 
@@ -48,7 +45,10 @@ def database_filter(legitimate_list, database):
 
 
 def delete_databases(db_name, db_host, root_password):
-    exclusion_list = ["information_schema", "mysql", "performance_schema", "ccsgoal","requestcentraldb","requestdesk_central_db_11","requestdesk_central_db_beta7","requestdesk_central_db_beta8","requestdesk_central_db_rc1","requestdesk_central_db_rc2","requestdesk_centraldb10release","requestdesk_centraldb_11_release","requestdesk_centraldb_1_0_release"]
+    exclusion_list = ["information_schema", "mysql", "performance_schema", "ccsgoal", "requestcentraldb",
+                      "requestdesk_central_db_11", "requestdesk_central_db_beta7", "requestdesk_central_db_beta8",
+                      "requestdesk_central_db_rc1", "requestdesk_central_db_rc2", "requestdesk_centraldb10release",
+                      "requestdesk_centraldb_11_release", "requestdesk_centraldb_1_0_release"]
     if db_name not in exclusion_list:
         # db_connection = pymysql.connect(db_host, "root", root_password)
         # db_pointer = db_connection.cursor()
@@ -72,7 +72,6 @@ def main():
         db_name = db[0]
         if database_filter(database_array, db_name):
             delete_databases(db_name, db_host, root_password)
-            # print(db[0])
     return True
 
 
