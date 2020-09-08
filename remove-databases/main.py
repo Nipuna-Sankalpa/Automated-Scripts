a73f012224f2681dac0ffb9ae51525d3e4e3394e
Nipuna-Sankalpa <nipunasankalpa.12@cse.mrt.ac.lk>
2020-09-08 21:03:36 +0530

    complete script with dry run mode

--- remove-databases/main.py
+++ remove-databases/main.py
@@ -1,5 +1,6 @@
 import os
 import yaml
+import pymysql
 
 
 def get_legitimate_db_list(webroot):
@@ -16,12 +17,11 @@ def get_legitimate_db_list(webroot):
                     isExist = os.path.exists(db_yml_path)
                     if isExist:
                         with open(db_yml_path, "r") as configuration:
-                            db_settings = yaml.load(configuration)
+                            db_settings = yaml.load(configuration, yaml.SafeLoader)
                             try:
                                 tmp = db_settings['all']['doctrine']['param']['dsn']
                                 literals = tmp.split(";")
                                 db_name = literals[2].split("=")[1]
-                                print(db_name)
                                 db_array.append(db_name)
                             except yaml.YAMLError as error:
                                 print(error)
@@ -30,20 +30,45 @@ def get_legitimate_db_list(webroot):
 
 
 def get_total_db_list(db_host, root_password):
-    return True
+    db_connection = pymysql.connect(db_host, "root", root_password)
+    db_pointer = db_connection.cursor()
+    db_pointer.execute("show databases")
+    result = db_pointer.fetchall()
+    db_connection.close()
+    return result
 
 
-def get_isolated_databases(legitimate_list, total_list):
-    return True
+def database_filter(legitimate_list, database):
+    if database not in legitimate_list:
+        return True
+    return False
 
 
-def database_filter(db_name, db_host, root_password):
-    return True
+def delete_databases(db_name, db_host, root_password):
+    exclusion_list = ["information_schema", "mysql", "performance_schema", "ccsgoal"]
+    if db_name not in exclusion_list:
+        # db_connection = pymysql.connect(db_host, "root", root_password)
+        # db_pointer = db_connection.cursor()
+        # db_pointer.execute("drop database " + db_name)
+        # result = db_pointer.fetchone()
+        # db_connection.close()
+        print(db_name)
+        # return result
+    return False
 
 
 def main():
-    database_array = get_legitimate_db_list("/var/lib/docker/workspace/infinity/html/OHRMStandalone")
-    print(database_array)
+    db_host = "172.40.0.102"
+    root_password = "p2835GmVeBpbPLT"
+    web_root = "/var/lib/docker/workspace/infinity/html/OHRMStandalone"
+
+    database_array = get_legitimate_db_list(web_root)
+    total_db_list = get_total_db_list(db_host, root_password)
+    for db in total_db_list:
+        db_name = db[0]
+        if database_filter(database_array, db_name):
+            delete_databases(db_name, db_host, root_password)
+            # print(db[0])
     return True
 
 
