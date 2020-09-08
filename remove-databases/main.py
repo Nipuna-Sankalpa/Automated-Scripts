91d8543a861bb54f9a0f95e35e0305a4e46cf1bd
Nipuna-Sankalpa <nipunasankalpa.12@cse.mrt.ac.lk>
2020-09-08 19:53:38 +0530

    merge fixed in directory paths

--- remove-databases/main.py
+++ remove-databases/main.py
@@ -7,21 +7,24 @@ def get_legitimate_db_list(webroot):
     isExist = os.path.exists(webroot)
     if not isExist:
         return "Web root does not exist"
-    for dirname, dirnames, filenames in os.walk(webroot):
-        for directory in dirnames:
-            db_yml_path = webroot + '/' + directory + "/symfony/config/database.yml"
-            isExist = os.path.exists(db_yml_path)
-            if isExist:
-                with open(db_yml_path, "r") as configuration:
-                    db_settings = yaml.load(configuration, Loader=yaml.FullLoader)
-                    try:
-                        tmp = db_settings['all']['doctrine']['param']['dsn']
-                        literals = tmp.split(";")
-                        db_name = literals[2].split("=")[1]
-                        print(db_name)
-                        db_array.append(db_name)
-                    except yaml.YAMLError as error:
-                        print(error)
+    for dirname, dirnames_first_level, filenames in os.walk(webroot):
+        for directory_first_level in dirnames_first_level:
+            secondary_webroot = webroot + '/' + directory_first_level
+            for dirname, secondary_directory, filenames in os.walk(secondary_webroot):
+                for directory in secondary_directory:
+                    db_yml_path = webroot + '/' + directory_first_level + '/' + directory + "/symfony/config/database.yml"
+                    isExist = os.path.exists(db_yml_path)
+                    if isExist:
+                        with open(db_yml_path, "r") as configuration:
+                            db_settings = yaml.load(configuration)
+                            try:
+                                tmp = db_settings['all']['doctrine']['param']['dsn']
+                                literals = tmp.split(";")
+                                db_name = literals[2].split("=")[1]
+                                print(db_name)
+                                db_array.append(db_name)
+                            except yaml.YAMLError as error:
+                                print(error)
 
     return db_array
 
